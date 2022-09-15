7faca397c12c7e90540fd0afed25c242411a1c29
Amal Rajan <amalrajan74@gmail.com>
2022-09-15 13:13:30 +0530

    Updates

--- app.go
+++ app.go
@@ -3,10 +3,11 @@ package main
 import (
     "io";
     "net/http";
+    "unit.nginx.org/go";
 )
 
 func main() {
     http.HandleFunc("/",func (w http.ResponseWriter, r *http.Request) {
-        io.WriteString(w, "Hello, Go on Unit!")
+        io.WriteString(w, "Hello, Go without the module!")
     })
 }
