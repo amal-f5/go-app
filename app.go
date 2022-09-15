1c8f7a7b226d2724e9db8f7f812f14d42be62907
Amal Rajan <amalrajan74@gmail.com>
2022-09-15 20:16:58 +0530

    Updates

--- app.go
+++ app.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		io.WriteString(w, "Hello, Go without the module!")
+		io.WriteString(w, "Hello, Go on Unit!")
 	})
 	http.ListenAndServe(":8080", nil)
 }
