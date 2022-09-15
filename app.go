0d12355a68595e4cb9f3cd963d60ab3fd8f87c41
Amal Rajan <amalrajan74@gmail.com>
2022-09-15 20:18:23 +0530

    Updates

--- app.go
+++ app.go
@@ -1,14 +1,15 @@
 package main
 
 import (
-    "io";
-    "net/http";
-    "unit.nginx.org/go"
+	"io"
+	"net/http"
+
+	"unit.nginx.org/go"
 )
 
 func main() {
-    http.HandleFunc("/",func (w http.ResponseWriter, r *http.Request) {
-        io.WriteString(w, "Hello, Go on Unit!")
-    })
-    unit.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "Hello, Go on Unit!")
+	})
+	unit.ListenAndServe(":8080", nil)
 }
